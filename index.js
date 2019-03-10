0fd181e1b3574ecb6aa4aafa9c7933be15eaea53
Yudha Pratama <yudha.webdev@gmail.com>
2019-03-11 00:50:27 +0700

    Added schema builder table

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const Builder = require('./lib/Builder');
 
 module.exports = Builder;
+
+Builder
+  .schema()
+  .withSchema('world')
+  .createTable('users')
+  .tableIncrement()
+  .tableString('name', 255)
+  .tableString('username', 50)
+  .tableString('phone', 14)
+  .runSchema()
+  .then((results) => {
+    console.log(results);
+  });
