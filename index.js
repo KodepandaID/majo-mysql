5f15ad95d538ba0c18a5a12e793730147538db0c
Yudha Pratama <yudha.webdev@gmail.com>
2019-03-11 13:55:19 +0700

    Fix

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 const Builder = require('./lib/Builder');
 
 module.exports = Builder;
-
-Builder
-  .schema()
-  .withSchema('world')
-  .createTable('users')
-  .tableIncrement()
-  .tableString('name', 255)
-  .tableString('username', 50)
-  .tableString('phone', 14)
-  .runSchema()
-  .then((results) => {
-    console.log(results);
-  });
