e761cc947111acb0f3c2ed3052d240447394ea44
Yudha Pratama <yudha.webdev@gmail.com>
2019-04-16 18:22:01 +0700

    Added testConnection() method

--- test/test_db_manager.js
+++ test/test_db_manager.js
@@ -6,6 +6,18 @@ const Majo = require('../index').connection({
 });
 
 describe('Majo Mysql DB Manager Testing', () => {
+  it('Run testConnection() method', (done) => {
+    Majo
+      .db()
+      .testConnection()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
   it('Run showDatabases() method', (done) => {
     Majo
       .db()
